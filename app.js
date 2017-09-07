d615cb3236c12a6941e31019d7e59590d61e678d
exenone <seeunuwan@gmail.com>
2017-09-07 14:23:29 +0200

    /inital commit

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+const express=require('express');
+const app=express();
 
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+app.get('/',function(req,res){
+    res.send('welcome!');
+  
+  });
+  
+  app.get('/routing',function(req,res){
+    res.send('welcome!its routing');
+  
+  });
+  app.listen(port, hostname, (err) => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+  
