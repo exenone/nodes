cbffcefa208fc608d88a693aedfbb5dbb003316b
exenone <seeunuwan@gmail.com>
2017-09-07 15:14:54 +0200

    /initial commit

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+
 app.get('/',function(req,res){
     res.send('welcome!');
   
@@ -15,6 +20,12 @@ app.get('/',function(req,res){
     res.send('welcome!its routing');
   
   });
+
+  app.all('/secret', function (req, res, next) {
+    console.log('Accessing the secret section ...')
+    next() 
+  });
+
   app.listen(port, hostname, (err) => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
